70b89b56e86e0f359bfc8af7fadd4b5fad59168c
Hatice Nur KAYA <66225075+HNK-hub@users.noreply.github.com>
2022-12-08 18:08:54 +0300

    Update testjunit.java

--- ebebek/src/test/java/com/example/ebebek/Test/testjunit.java
+++ ebebek/src/test/java/com/example/ebebek/Test/testjunit.java
@@ -22,7 +22,7 @@ import org.openqa.selenium.Keys;
 import java.util.*;
 import java.net.MalformedURLException;
 import java.net.URL;
-public class Test12Test {
+public class testjunit {
   private WebDriver driver;
   private Map<String, Object> vars;
   JavascriptExecutor js;
